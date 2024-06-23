cc1a8c9a938fcc246aa8b8fb88d7150f0fc776a2
nhonoree <honoreniyirema@gmail.com>
2024-06-23 15:08:11 -0700

    102-print_comb5

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+	* main - Entry point
+	*
+	* Return: Always 0 (Success)
+	*/
+int main(void)
+{
+	int num1_tens, num1_ones, num2_tens, num2_ones;
+
+	for (num1_tens = 0; num1_tens <= 9; num1_tens++)
+	{
+	for (num1_ones = 0; num1_ones <= 9; num1_ones++)
+	{
+	for (num2_tens = num1_tens; num2_tens <= 9; num2_tens++)
+	{
+	int start_num2_ones = (num2_tens == num1_tens) ? num1_ones + 1 : 0;
+
+	for (num2_ones = start_num2_ones; num2_ones <= 9; num2_ones++)
+	{
+	putchar(num1_tens + '0');
+	putchar(num1_ones + '0');
+	putchar(' ');
+	putchar(num2_tens + '0');
+	putchar(num2_ones + '0');
+
+	if (!(num1_tens == 9 && num1_ones == 9 && num2_tens == 9 && num2_ones == 9))
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	}
+	}
+	}
+	putchar('\n');
+
+	return (0);
+}
