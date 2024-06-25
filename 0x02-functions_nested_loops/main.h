0fd46a74d54cf9addc08d6d7d49ad005ff667aa5
nhonoree <honoreniyirema@gmail.com>
2024-06-25 11:34:24 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,6 @@
-#ifndef MAIN_HEADER_FILE
-#define MAIN_HEADER_FILE
+#ifndef MAIN_H
+#define MAIN_H
+
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -8,9 +9,7 @@ int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
-void jack_last_digit(int);
+void jack_bauer(void);  // Add this line
 void times_table(void);
-int add(int,int);
-void print_to_98(int n);
 
-#endif
+#endif /* MAIN_H */
