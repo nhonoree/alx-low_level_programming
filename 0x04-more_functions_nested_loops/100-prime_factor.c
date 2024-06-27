9ef9020cb7742131013178934626ef7058b2b0b7
nhonoree <honoreniyirema@gmail.com>
2024-06-27 03:29:35 -0700

    updates

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -8,30 +8,30 @@
  */
 int main(void)
 {
-    long num = 612852475143;
-    long max_prime = -1;
-    long i;
+	long num = 612852475143;
+	long max_prime = -1;
+	long i;
 
-    while (num % 2 == 0)
-    {
-        max_prime = 2;
-        num /= 2;
-    }
+	while (num % 2 == 0)
+	{
+		max_prime = 2;
+		num /= 2;
+	}
 
-    for (i = 3; i <= sqrt(num); i += 2)
-    {
-        while (num % i == 0)
-        {
-            max_prime = i;
-            num /= i;
-        }
-    }
+	for (i = 3; i <= sqrt(num); i += 2)
+	{
+		while (num % i == 0)
+		{
+			max_prime = i;
+			num /= i;
+		}
+	}
 
-    if (num > 2)
-        max_prime = num;
+	if (num > 2)
+		max_prime = num;
 
-    printf("%ld\n", max_prime);
+	printf("%ld\n", max_prime);
 
-    return (0);
+	return (0);
 }
 
--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,22 +1,22 @@
 #include "main.h"
 
 /**
- * print_number - Prints an integer
- * @n: The integer to print
+ * print_number - prints an integer
+ * @n: integer to be printed
  */
 void print_number(int n)
 {
-    unsigned int num = n;
+	unsigned int num = n;
 
-    if (n < 0)
-    {
-        _putchar('-');
-        num = -num;
-    }
+	if (n < 0)
+	{
+		_putchar('-');
+		num = -n;
+	}
 
-    if (num / 10)
-        print_number(num / 10);
+	if ((num / 10) > 0)
+		print_number(num / 10);
 
-    _putchar((num % 10) + '0');
+	_putchar((num % 10) + '0');
 }
 
