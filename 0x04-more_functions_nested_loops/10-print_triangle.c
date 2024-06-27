1cc576c84ade6c2b3e0ac3f169b95bd170e51f5d
nhonoree <honoreniyirema@gmail.com>
2024-06-26 22:57:06 -0700

    updates

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -6,24 +6,24 @@
  */
 void print_triangle(int size)
 {
-    int i, j;
+	int i, j;
 
-    if (size <= 0)
-    {
-        _putchar('\n');
-    }
-    else
-    {
-        for (i = 1; i <= size; i++)
-        {
-            for (j = 1; j <= size; j++)
-            {
-                if (j <= size - i)
-                    _putchar(' ');
-                else
-                    _putchar('#');
-            }
-            _putchar('\n');
-        }
-    }
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 1; j <= size; j++)
+			{
+				if (j <= size - i)
+					_putchar(' ');
+				else
+					_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
 }
