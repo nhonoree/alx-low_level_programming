9c9e9d95fb21a97ffc4fa2289d285e730f90a7fb
nhonoree <honoreniyirema@gmail.com>
2024-06-26 23:44:54 -0700

    updates

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -11,18 +11,53 @@
  */
 void print_remaining_days(int month, int day, int year)
 {
-	int days_in_months[] = {0, 31, 59, 90, 120, 151, 181,
-				212, 243, 273, 304, 334};
+	/* Adjusting day based on month and year */
+	switch (month)
+	{
+		case 2:
+			day = 31 + day;
+			break;
+		case 3:
+			day = 59 + day;
+			break;
+		case 4:
+			day = 90 + day;
+			break;
+		case 5:
+			day = 120 + day;
+			break;
+		case 6:
+			day = 151 + day;
+			break;
+		case 7:
+			day = 181 + day;
+			break;
+		case 8:
+			day = 212 + day;
+			break;
+		case 9:
+			day = 243 + day;
+			break;
+		case 10:
+			day = 273 + day;
+			break;
+		case 11:
+			day = 304 + day;
+			break;
+		case 12:
+			day = 334 + day;
+			break;
+		default:
+			break;
+	}
 
-	if (month < 1 || month > 12 || day < 1 || day > 31)
-		return;
-
-	day = days_in_months[month - 1] + day;
-
+	/* Checking for leap year and adjusting day count */
 	if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
 	{
 		if (month > 2)
+		{
 			day++;
+		}
 
 		printf("Day of the year: %d\n", day);
 		printf("Remaining days: %d\n", 366 - day);
@@ -30,7 +65,9 @@ void print_remaining_days(int month, int day, int year)
 	else
 	{
 		if (month == 2 && day == 60)
+		{
 			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+		}
 		else
 		{
 			printf("Day of the year: %d\n", day);
