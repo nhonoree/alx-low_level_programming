0f8a21e9b14f9bd17282c36ab2b17188e9645769
nhonoree <honoreniyirema@gmail.com>
2024-07-01 00:26:37 -0700

    updates

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -2,29 +2,49 @@
 
 /**
  * _atoi - Converts a string to an integer.
- * @s: The string to be converted.
+ * @s: The string to convert.
  *
- * Return: The integer value of the string.
+ * Return: The integer value of the converted string.
  */
 int _atoi(char *s)
 {
-	int i = 0;
-	int sign = 1;
-	int result = 0;
-	int started = 0;
+    int result = 0;
+    int sign = 1;
+    int i = 0;
 
-	while (s[i] != '\0')
-	{
-		if (s[i] == '-' && !started)
-			sign *= -1;
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			started = 1;
-			result = result * 10 + (s[i] - '0');
-		}
-		else if (started)
-			break;
-		i++;
-	}
-	return (sign * result);
+    // Skip leading whitespace
+    while (s[i] == ' ')
+        i++;
+
+    // Check sign
+    if (s[i] == '-')
+    {
+        sign = -1;
+        i++;
+    }
+    else if (s[i] == '+')
+    {
+        i++;
+    }
+
+    // Convert digits to integer
+    while (s[i] >= '0' && s[i] <= '9')
+    {
+        int digit = s[i] - '0';
+
+        // Check for overflow before updating result
+        if (sign == 1 && (result > INT_MAX / 10 || (result == INT_MAX / 10 && digit > INT_MAX % 10)))
+        {
+            return INT_MAX; // Return maximum possible value
+        }
+        else if (sign == -1 && (result > -(INT_MIN / 10) || (result == -(INT_MIN / 10) && digit > -(INT_MIN % 10))))
+        {
+            return INT_MIN; // Return minimum possible value
+        }
+
+        result = result * 10 + digit;
+        i++;
+    }
+
+    return result * sign;
 }
