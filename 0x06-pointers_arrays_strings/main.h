544a96bc0555055f1a8b46beac7cb23928affde3
nhonoree <honoreniyirema@gmail.com>
2024-07-03 08:54:39 -0700

    updates

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -14,5 +14,9 @@ char *rot13(char *s);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
+int _putchar(char c);
+void print_number(int n);
+int _putchar(char c);
+void print_number(int n);
 
 #endif /* MAIN_H */
