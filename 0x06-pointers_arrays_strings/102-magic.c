5e4e977def3bcb5c6180ddb3f640ad7d53d3dea0
nhonoree <honoreniyirema@gmail.com>
2024-07-03 10:17:24 -0700

    updates

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
@@ -8,16 +13,18 @@ int main(void)
 
 	a[2] = 1024;
 	p = &n;
+
 	/*
-	 * write your line of code here...
-	 * Rember:
-	 * - you are not allowed to use a
-	 * - you are not alowed to modify p
-	 * - only one statement
-	 * - you are not allowed to code anything else than this line of code
+	 * Write your line of code here...
+	 * Remember:
+	 * - You are not allowed to use 'a'
+	 * - You are not allowed to modify 'p'
+	 * - Only one statement
+	 * - You are not allowed to code anything else than this line of code
 	 */
-	*(p + 5) = 98;
+	*(p + (sizeof(int) * 2)) = 98; /* This line modifies p[2] */
+
 	/* ...so that this prints 98\n */
-	printf("a[2] = %f\n", a[2]);
+	printf("a[2] = %d\n", a[2]);
 	return (0);
 }
