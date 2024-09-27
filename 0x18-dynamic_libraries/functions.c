e470a0e1e47ae67ee1bfdc59faf77f1496a6fbc7
nhonoree <honoreniyirema@gmail.com>
2024-09-27 14:49:30 -0700

    updates

--- 0x18-dynamic_libraries/functions.c
+++ 0x18-dynamic_libraries/functions.c
@@ -1,41 +1,20 @@
-#include "main.h"
-
-int _putchar(char c) { return (0); }
-
-int _islower(int c) { return (0); }
-
-int _isalpha(int c) { return (0); }
-
-int _abs(int n) { return (0); }
-
-int _isupper(int c) { return (0); }
-
-int _isdigit(int c) { return (0); }
-
-int _strlen(char *s) { return (0); }
-
-void _puts(char *s) {}
-
-char *_strcpy(char *dest, char *src) { return (dest); }
-
-int _atoi(char *s) { return (0); }
-
-char *_strcat(char *dest, char *src) { return (dest); }
-
-char *_strncat(char *dest, char *src, int n) { return (dest); }
-
-char *_strncpy(char *dest, char *src, int n) { return (dest); }
-
-int _strcmp(char *s1, char *s2) { return (0); }
-
-char *_memset(char *s, char b, unsigned int n) { return (s); }
-
-char *_memcpy(char *dest, char *src, unsigned int n) { return (dest); }
-
-char *_strchr(char *s, char c) { return (s); }
-
-unsigned int _strspn(char *s, char *accept) { return (0); }
-
-char *_strpbrk(char *s, char *accept) { return (s); }
-
-char *_strstr(char *haystack, char *needle) { return (haystack); }
+int _putchar(char c) { (void)c; return (0); }
+int _islower(int c) { (void)c; return (0); }
+int _isalpha(int c) { (void)c; return (0); }
+int _abs(int n) { (void)n; return (0); }
+int _isupper(int c) { (void)c; return (0); }
+int _isdigit(int c) { (void)c; return (0); }
+int _strlen(char *s) { (void)s; return (0); }
+void _puts(char *s) { (void)s; }
+char *_strcpy(char *dest, char *src) { (void)src; return (dest); }
+int _atoi(char *s) { (void)s; return (0); }
+char *_strcat(char *dest, char *src) { (void)src; return (dest); }
+char *_strncat(char *dest, char *src, int n) { (void)src; (void)n; return (dest); }
+char *_strncpy(char *dest, char *src, int n) { (void)src; (void)n; return (dest); }
+int _strcmp(char *s1, char *s2) { (void)s1; (void)s2; return (0); }
+char *_memset(char *s, char b, unsigned int n) { (void)b; (void)n; return (s); }
+char *_memcpy(char *dest, char *src, unsigned int n) { (void)src; (void)n; return (dest); }
+char *_strchr(char *s, char c) { (void)c; return (s); }
+unsigned int _strspn(char *s, char *accept) { (void)s; (void)accept; return (0); }
+char *_strpbrk(char *s, char *accept) { (void)accept; return (s); }
+char *_strstr(char *haystack, char *needle) { (void)needle; return (haystack); }
