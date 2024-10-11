dfa8e7f21352bfc7d83002ccb15e3ea11df49071
nhonoree <honoreniyirema@gmail.com>
2024-10-11 14:02:26 -0700

    updates

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,30 +1,29 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_create - Creates a hash table
- * @size: Size of the array
+ * hash_table_create - Creates a hash table.
+ * @size: The size of the array.
  *
- * Return: Pointer to the newly created hash table
+ * Return: A pointer to the newly created hash table.
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hash_table;
-	unsigned long int i;
+	hash_node_t **array;
 
-	hash_table = malloc(sizeof(hash_table_t));
-	if (hash_table == NULL)
+	hash_table = malloc(sizeof(hash_table_t));  /* No space before '(' */
+	if (!hash_table)
 		return (NULL);
 
-	hash_table->size = size;
-	hash_table->array = malloc(sizeof(hash_node_t *) * size);
-	if (hash_table->array == NULL)
+	array = malloc(sizeof(hash_node_t *) * size);  /* No space before '(' */
+	if (!array)
 	{
-		free(hash_table);
+		free(hash_table);  /* No space before '(' */
 		return (NULL);
 	}
 
-	for (i = 0; i < size; i++)
-		hash_table->array[i] = NULL;
+	hash_table->size = size;
+	hash_table->array = array;
 
-	return (hash_table);
+	return (hash_table);  /* No space before '(' */
 }
