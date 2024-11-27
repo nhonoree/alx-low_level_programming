a55cb9f8e5b55ecc479871fc4544875fc0c76066
nhonoree <honoreniyirema@gmail.com>
2024-11-27 14:19:06 -0800

    task 0

---
+++ 0x1C-makefiles/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void print_school(void);
+
+/**
+ * main - Entry point of the program
+ * 
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    print_school();
+    return (0);
+}
